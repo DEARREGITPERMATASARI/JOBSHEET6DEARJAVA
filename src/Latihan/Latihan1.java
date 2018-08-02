21a3bee3f82b279b2a3d6381776b71f956ee0ebb
Nita <Nita@DESKTOP-PHVQM3J>
2018-08-02 22:03:00 +0700

    Coba IF

--- src/Latihan/Latihan1.java
+++ src/Latihan/Latihan1.java
@@ -5,13 +5,24 @@
  */
 package Latihan;
 
-import java.util.Scanner;
-
+/**
+ *
+ * @author Nita
+ */import java.util.Scanner;
+public class Latihan1 {
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ ;
+package Latihan;
 /**
  *
  * @author Nita
  */
-public class Latihan1 {
+
+
+
     public static void main(String[] args) {
         String identitas = "nama lengkap / kelas/ no";
         System.out.println("Identitas : " + identitas);
