8bc9bd994cadcfe640fccd7e857b72221d9e7bd7
Nita <Nita@DESKTOP-PHVQM3J>
2018-08-03 19:01:09 +0700

    Tambah Input Bahan 2

--- src/Latihan/SaranBahan2Resep.java
+++ src/Latihan/SaranBahan2Resep.java
@@ -5,21 +5,49 @@
  */
 package Latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Nita
- */
-public class SaranBahan2Resep {
-    public static void main(String[] args ) {
+ */public class SaranBahan2Resep {
+    public static void main(String[] args )
+    {
+   
     String identitas = "DEAR REGITA PERMATASARI / X RPL 5 / 11 ";
     System.out.println ("Identitas : " + identitas);
-        System.out.print ("\nSaran Reseo dari Bahan Milik Anda\n");
+        System.out.print ("\nSaran Resep dari Bahan Milik Anda\n");
         
-        System.out.println("Bahan Pertama");
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Bahan Pertama : ");
         System.out.println("1. Pisang");
         System.out.println("2. Telur");
-            System.out.print ("Masukkan no pilihan Anda :") ;
-            int bahanl = scanner.nextInt();
+        System.out.print ("Masukkan no pilihan Anda :") ;
+            int bahan1 = scanner.nextInt();
             
-}
-}
+            
+if(bahan1 == 1 || bahan1 == 2);
+{
+    System.out.println("Bahan Kedua: ");
+    if (bahan1 == 1) {
+            
+        System.out.println("1. Susu");
+        System.out.println ("2. Minyak Goreng");
+        System.out.println ("3. Tidak Ada");
+    }else{
+        
+        System.out.println("1. Minyak Goreng");
+        System.out.println("2. Roti");
+        System.out.println("3. Tidak Ada");
+    }
+    
+    System.out.print("Masukkan no pilihan Anda :");
+    int bahan2 = scanner.nextInt();
+
+   
+} else
+    System.out.println("Mohon Maaf, Pilihan tidak ditemukan, "
+            + "tidak dapat memberikan saran resep");
+    }
+ }
