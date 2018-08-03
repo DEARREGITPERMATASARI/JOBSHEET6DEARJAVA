e282f9d27a6754bb1c5c998c57b10af30249e2c9
Nita <Nita@DESKTOP-PHVQM3J>
2018-08-03 18:27:58 +0700

    Initial Commit

---
+++ src/Latihan/SaranBahan2Resep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Nita
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args ) {
+    String identitas = "DEAR REGITA PERMATASARI / X RPL 5 / 11 ";
+    System.out.println ("Identitas : " + identitas);
+        System.out.print ("\nSaran Reseo dari Bahan Milik Anda\n");
+        
+    
+}
+}
