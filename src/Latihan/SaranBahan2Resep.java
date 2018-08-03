f884b9249e6a0f6486b7abc7870da86f273ef31d
Nita <Nita@DESKTOP-PHVQM3J>
2018-08-03 19:26:42 +0700

    Tambah Tampil Resep

--- src/Latihan/SaranBahan2Resep.java
+++ src/Latihan/SaranBahan2Resep.java
@@ -4,50 +4,65 @@
  * and open the template in the editor.
  */
 package Latihan;
-
 import java.util.Scanner;
-
 /**
  *
  * @author Nita
- */public class SaranBahan2Resep {
-    public static void main(String[] args )
-    {
-   
-    String identitas = "DEAR REGITA PERMATASARI / X RPL 5 / 11 ";
-    System.out.println ("Identitas : " + identitas);
-        System.out.print ("\nSaran Resep dari Bahan Milik Anda\n");
+ */
+public class SaranBahan2Resep {
+        public static void main(String[] args) {
+        String identitas = "Salsabila Fawnia Ramadhani Khoirunisa / XRPL5 / 40";
+        System.out.println("Identitas : "+ identitas);
         
-        Scanner scanner = new Scanner (System.in);
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
         
-        System.out.println("Bahan Pertama : ");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bahan Pertama: ");
         System.out.println("1. Pisang");
         System.out.println("2. Telur");
-        System.out.print ("Masukkan no pilihan Anda :") ;
-            int bahan1 = scanner.nextInt();
-            
-            
-if(bahan1 == 1 || bahan1 == 2);
-{
-    System.out.println("Bahan Kedua: ");
-    if (bahan1 == 1) {
-            
-        System.out.println("1. Susu");
-        System.out.println ("2. Minyak Goreng");
-        System.out.println ("3. Tidak Ada");
-    }else{
+        System.out.println("Masukkan no pilihan Anda :");
+        int bahan1 = scanner.nextInt();
         
-        System.out.println("1. Minyak Goreng");
-        System.out.println("2. Roti");
-        System.out.println("3. Tidak Ada");
-    }
-    
-    System.out.print("Masukkan no pilihan Anda :");
-    int bahan2 = scanner.nextInt();
+        if(bahan1 == 1 || bahan1 == 2)
+        {
+            System.out.println("Bahan Kedua: ");
+            if(bahan1 == 1) {
+                System.out.println("1. Susu");
+                System.out.println("2. Minyak Goreng");
+                System.out.println("3. Tidak ada");
+            }
+            System.out.print("Masukkan no pilihan Anda :");
+            int bahan2 = scanner.nextInt();
+             }else 
+            System.out.println("Mohon Maaf, Pilihan tidak Ditemukan, " + "tidak dapat memberikan saran resep");
+        int bahan2 = scanner.nextInt();
+        if(bahan2 >= 1 && bahan2 <= 3)
+        { 
+            if(bahan1==1)
+                switch(bahan2)
+                {
+                    case 1: System.out.println(
+                            "Anda dapat membuat milk shake banana"); break;
+                    case 2: System.out.println(
+                            "Anda dapat membuat pisang goreng"); break;
+                    case 3: System.out.println(
+                            "Anda dapat membuat pisang rebus");
+                }
+            else 
+                switch(bahan2)
+                {
+                        case 1: System.out.println(
+                                "Anda dapat membuat telur mata sapi"); break;
+                        case 2: System.out.println(
+                                "Anda dapat membuat sandwich telur"); break;
+                        case 3: System.out.println(
+                                "Anda dapat membuat telur rebus");
+                }
+        }else 
+            System.out.println("Mohon maaf, Pilihan tidak ditemukan, "
+                 + "tidak dapat Memberikan saran resep");
+            }
+        }
 
-   
-} else
-    System.out.println("Mohon Maaf, Pilihan tidak ditemukan, "
-            + "tidak dapat memberikan saran resep");
-    }
- }
+
